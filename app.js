fdb184629b5bd90aa2488022bb03fc0acfa42487
Ryan Barrett <git@ryanb.org>
2018-07-06 07:20:28 -0700

    migrate to node.js 😱 on app engine

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// [START app]
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END app]
